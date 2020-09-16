29d19413a6a31872598c45387986590a31adea95
evgenyantipin <evgeny.antipin@ikitlab.com>
2020-09-16 10:40:45 +0300

    add cookie support with next-cookies cookie-parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const polka = require("polka");
 const next = require("next");
+const cookieParser = require("cookie-parser");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
 process.env.PORT = process.env.PORT || 80;
@@ -9,7 +10,7 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = polka();
-
+  server.use(cookieParser());
   server.get("*", (req, res) => {
     return handle(req, res);
   });
@@ -18,13 +19,7 @@ app.prepare().then(() => {
     if (err) {
       throw err;
     }
-    console.log(
-      "> Ready on http://localhost:" +
-        process.env.PORT +
-        " [" +
-        process.env.NODE_ENV +
-        "]"
-    );
+    console.log(`> Ready on http://localhost:${process.env.PORT} [${process.env.NODE_ENV}]`);
   });
 
 });
