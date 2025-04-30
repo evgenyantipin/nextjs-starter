7556a21c3da791114a68343851d7a0f697b34c38
evgenyantipin <evgeny.antipin69@gmail.com>
2025-04-30 23:21:25 +0300

    chore: imrove post page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,32 +1,53 @@
+import { GetServerSideProps, NextPage } from 'next';
 import Layout from 'layouts/Main';
 import Carousel from 'components/Carousel';
-import AccordionDemo from 'components/Accordion'
+import AccordionDemo from 'components/Accordion';
 import { getPost } from 'api/posts';
 import styles from './Post.module.css';
 import { IPost } from 'types/IPost';
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async ({
+  params
+}) => {
+  const id = params?.id as string;
+
+  if (!id) {
+    return {
+      notFound: true
+    };
+  }
+
+  const postData: any = await getPost(id);
+
+  if (!postData?.length) {
+    return {
+      notFound: true
+    };
+  }
+
+  return {
+    props: {
+      post: postData?.[0]
+    }
+  };
+};
 
 type PageProps = {
   post: IPost;
 };
 
-export const getServerSideProps = async ({ params }: any) => {
-  const post: any = await getPost(params?.id as string);
-  return {
-    props: {
-      post: post[0]
-    }
-  };
+const PostPage: NextPage<PageProps> = (props) => {
+  const { post } = props;
+  return (
+    <Layout>
+      <div className={styles.post}>
+        <h1>{post.title}</h1>
+        <Carousel />
+        <p>{post.body}</p>
+        <AccordionDemo />
+      </div>
+    </Layout>
+  );
 };
-
-const PostPage = ({ post }: PageProps) => (
-  <Layout>
-    <div className={styles.post}>
-      <h1>{post.title}</h1>
-      <Carousel />
-      <p>{post.body}</p>
-      <AccordionDemo/>
-    </div>
-  </Layout>
-);
 
 export default PostPage;
